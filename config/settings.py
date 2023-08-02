d7080d12e3eb3ad5bcf23af15a466de480e6981c
Anton Vedischev <vedishchevanton@gmail.com>
2023-08-02 13:51:57 +0300

    настройки доступа

--- config/settings.py
+++ config/settings.py
@@ -141,11 +141,10 @@ REST_FRAMEWORK = {
     ],
     # Настройка прав доступа
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',  # без ограничений для всех включая анонимов
-        # 'rest_framework.permissions.IsAuthenticated' # перекрываем полностью всю работу от анонимных
+        'rest_framework.permissions.IsAuthenticated'  # перекрываем полностью всю работу от анонимных
         # (неавторизованных пользователей)
+        # 'rest_framework.permissions.AllowAny' # без ограничений для всех включая анонимов
     ]
-
 }
 
 # Настройки срока действия токенов
