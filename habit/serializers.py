0c48a09961cf3cb0baf5c604cf8e1228f031b587
Anton Vedischev <vedishchevanton@gmail.com>
2023-08-02 13:25:36 +0300

    настройки сериализатора привычки

--- habit/serializers.py
+++ habit/serializers.py
@@ -5,6 +5,7 @@ from users.models import User
 
 
 class HabitSerializer(serializers.ModelSerializer):
+    # Поле "user" является внешним ключом к модели User. Запрос к базе данных для получения всех пользователей.
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False)
 
     class Meta:
