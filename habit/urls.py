a205f09f8a62e607f46d33c844ae0cc25f88b40c
Anton Vedischev <vedishchevanton@gmail.com>
2023-08-02 13:51:28 +0300

    настройки урлов привычки

--- habit/urls.py
+++ habit/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
 from habit.apps import AppHabitConfig
-from habit.views import HabitCreateAPIView
+from habit.views import HabitCreateAPIView, HabitListAPIView, HabitRetrieveAPIView, HabitDestroyAPIView, \
+    HabitUpdateAPIView
 
 app_name = AppHabitConfig.name
 
 urlpatterns = [
     path('habit/create/', HabitCreateAPIView.as_view(), name='habit-create'),
+    path('habit/', HabitListAPIView.as_view(), name='habit-list'),
+    path('habit/<int:pk>/', HabitRetrieveAPIView.as_view(), name='habit-get'),
+    path('habit/update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habit-update'),
+    path('habit/delete/<int:pk>/', HabitDestroyAPIView.as_view(), name='habit-delete')
 
 ]
