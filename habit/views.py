73d5638080a6d39ba346ce927dde839d1a6e43f8
Anton Vedischev <vedishchevanton@gmail.com>
2023-08-02 13:50:37 +0300

    настройки CORS  привычки

--- habit/views.py
+++ habit/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
+from habit.models import Habit
 from habit.serializers import HabitSerializer
 
 
@@ -16,3 +17,32 @@ class HabitCreateAPIView(generics.CreateAPIView):
         user = self.request.user
         # Передаем пользователя в качестве значения поля user в создаваемой привычке
         serializer.save(user=user)
+
+
+class HabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.all()
+
+
+class HabitRetrieveAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.all()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Habit.objects.all()
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated]
