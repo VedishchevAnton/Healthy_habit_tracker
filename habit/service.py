6d6f0775808d965a3ec6e5450a644fac85a66fd9
Anton Vedischev <vedishchevanton@gmail.com>
2023-08-06 19:58:16 +0300

    установка Celery и Redis

--- habit/service.py
+++ habit/service.py
@@ -4,13 +4,26 @@ from config import settings
 
 telegram_api_token = settings.TELEGRAM_API_TOKEN
 chat_id = settings.CHAT_ID
-message = "Не забудь о своей привычке"
 
-url = f"https://api.telegram.org/bot{telegram_api_token}/sendMessage"
-params = {
-    "chat_id": chat_id,
-    "text": message
-}
 
-response = requests.get(url, params=params)
-print(response.json())
+# message = "Не забудь о своей привычке"
+#
+# url = f"https://api.telegram.org/bot{telegram_api_token}/sendMessage"
+# params = {
+#     "chat_id": chat_id,
+#     "text": message
+# }
+#
+# response = requests.get(url, params=params)
+# print(response.json())
+
+
+def send_telegram_message(habit):
+    message = f"Привычка для пользователя {habit.user.telegram_username}:\nМесто: {habit.place}\nДействие: {habit.action}\nВремя: {habit.time}"
+    url = f"https://api.telegram.org/bot{telegram_api_token}/sendMessage"
+    params = {
+        "chat_id": chat_id,
+        "text": message
+    }
+    response = requests.get(url, params=params)
+    return response.json()
