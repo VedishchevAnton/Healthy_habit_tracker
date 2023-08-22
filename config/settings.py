3cc5e20f3a50bd5c3b7c1690a8aa118fc791df93
VedishchevAnton <vedishchevanton@gmail.com>
2023-08-22 23:05:42 +0300

    реализация докера

--- config/settings.py
+++ config/settings.py
@@ -100,6 +100,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'mysecretpassword',
+#         'HOST': 'db',
+#
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
